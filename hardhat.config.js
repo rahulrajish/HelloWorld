654c8a592641c3fe6d91a90393525f2849aa2df2
Rahul Rajish Nathan <74354544+rahulrajish@users.noreply.github.com>
2022-01-28 22:08:25 +0530

    Adding changes to add the code to etherscan
    Adding changes to add the code to etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 
 require("@nomiclabs/hardhat-ethers");
 
+require("@nomiclabs/hardhat-etherscan");
+
 const {API_URL, PRIVATE_KEY} = process.env;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -17,4 +20,7 @@ module.exports = {
       accounts : [`0x${PRIVATE_KEY}`]
     }
   },
+  etherscan : {
+    apiKey: ETHERSCAN_API_KEY
+  }
 };
